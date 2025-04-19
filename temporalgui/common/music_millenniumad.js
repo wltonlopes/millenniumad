925eabfab9575056317a9fcd71533c601ffc3311
wltonlopes <wltonlopesmetal@gmail.com>
2025-04-19 12:37:31 -0300

    atualizações

---
+++ temporalgui/common/music_millenniumad.js
@@ -0,0 +1,11 @@
+Music.prototype.resetTracks = function()
+{
+	this.tracks = {
+		"MENU": ["heroic-story-inspiring-theme-9716.ogg"],
+		"PEACE": [],
+		"BATTLE": ["Valor.ogg", "Through_Enemy_Lines.ogg"],
+		"VICTORY": ["On_the_Road_to_Adventures.ogg"],
+		"DEFEAT": ["Ominous_Revelations.ogg"],
+		"CUSTOM": []
+	};
+};
